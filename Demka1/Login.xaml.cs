341a26466adcf30111baf072a7cf879f8f54e897
Ivan7 <Ivan7@DESKTOP-SRTCC3M>
2022-02-10 23:18:36 +0300

    Добавьте файлы проекта.

---
+++ Demka1/Login.xaml.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+using Demka1.DBSetTableAdapters;
+
+namespace Demka1
+{
+    public partial class Login : Window
+    {
+        DBSet dbSet = new DBSet();
+        UsersTableAdapter usersTableAdapter = new UsersTableAdapter();
+        public Login()
+        {
+            InitializeComponent();
+        }
+        private void Btn_Login_Click(object sender, RoutedEventArgs e)
+        {
+            if (Username.Text != "" && Password.Password != "")
+            {
+                int? id_user = usersTableAdapter.Login(Username.Text, Password.Password);
+                if(id_user == null)
+                {
+                    ExeptionBlock.Text = "Пользователь не найден";
+                }
+                else if(id_user != null)
+                {
+                    ExeptionBlock.Text = "Пользователь найден!";
+                }
+            }
+            else
+                ExeptionBlock.Text = "Поле логина или пароля пусто!";
+        }
+        private void Btn_Exit_Click(object sender, RoutedEventArgs e)
+        {
+            this.Close();
+        }
+    }
+}
